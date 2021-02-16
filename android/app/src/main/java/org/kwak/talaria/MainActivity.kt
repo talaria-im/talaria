c3643a30d90ecaba74fe6ff816375b291f352426
expectocode <expectocode@gmail.com>
2021-02-16 20:46:37 +0000

    Call Rust from Kotlin!

--- android/app/src/main/java/org/kwak/talaria/MainActivity.kt
+++ android/app/src/main/java/org/kwak/talaria/MainActivity.kt
@@ -2,10 +2,24 @@ package org.kwak.talaria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
+external fun hello(to: String): String
+
+fun loadTestLib() {
+    System.loadLibrary("test_lib")
+}
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Load Rust
+        loadTestLib()
+        findViewById<TextView>(R.id.txtMessage).let {
+            it?.text =  hello("Talaria")
+        }
     }
 }
--- test_lib/src/lib.rs
+++ test_lib/src/lib.rs
@@ -1,7 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![cfg(target_os = "android")]
+#![allow(non_snake_case)]
+
+use jni::objects::{JClass, JString};
+use jni::sys::jstring;
+use jni::JNIEnv;
+
+#[no_mangle]
+pub extern "system" fn Java_org_kwak_talaria_MainActivityKt_hello(
+  env: JNIEnv,
+  _: JClass,
+  input: JString,
+) -> jstring {
+  let input: String = env
+    .get_string(input)
+    .expect("Couldn't get Java string!")
+    .into();
+  let output = env
+    .new_string(format!("Hello from Rust: {}", input))
+    .expect("Couldn't create a Java string!");
+  output.into_inner()
 }
